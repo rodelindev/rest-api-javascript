cd58cbc4686454e99ef1ae241f7e09857478b37e
RodelinDipre <rdbivieca@gmail.com>
2020-07-01 20:58:02 -0400

    Modelo de ususario y su index de inicio.

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userShema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model('User', userShema);
+
+module.exports = User;
