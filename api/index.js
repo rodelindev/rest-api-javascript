9b943a823027e2c829fe8acababbdf12ac8506f7
RodelinDipre <rdbivieca@gmail.com>
2020-07-01 21:50:16 -0400

    Agregando el CRUD del index en la carpeta api

--- api/index.js
+++ api/index.js
@@ -8,4 +8,25 @@ api.use(express.json({ extended: true }));
 
 api.get('/', (req, res) => res.send('Hello World'));
 
+// CRUD Usuarios
+const { User } = require('../models');
+
+// JS Vanilla
+// api.post('/api/v1/users', (req, res) => {
+//   new User(req.body)
+//     .save()
+//     .then((user) => res.status(201).json(user))
+//     .catch((err) => res.status(400).json(err));
+// });
+
+// ES6
+api.post('/api/v1/users', async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = { api, PORT };
